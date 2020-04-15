121cf5a9aa6e66acc67ec276acc4ae93a5aeca78
KentFletcher <kentfletch09@gmail.com>
2020-04-15 11:34:26 -0700

    added location api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
+const superagent = require('superagent');
 const cors = require('cors');
 const app = express();
 
@@ -14,21 +15,36 @@ app.get('/hello', (request, response) => {
 });
 
 app.get('/location', (request, response) => {
-  const geoData = require('./data/geo.json');
+//   const geoData = require('./data/geo.json');
   const city = request.query.city;
-  const display = new Location(city, geoData[0]);
-  response.status(200).json(display);
+  const key = process.env.GEOCODE_API_KEY;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+
+  superagent.get(url)
+    .then(locationResponse => {
+      const data = locationResponse.body;
+      for (var i in data) {
+        if (data[i].display_name.search(city)) {
+          const display = new Location(city, data[i]);
+          response.send(display);
+        }
+      }
+    })
+    .catch(error => {
+      handleError(error, response);
+    });
 });
+
 
 app.get('/weather', (request, response) => {
   const weather = require('./data/darksky.json');
   const weatherArray = weather.daily.data;
-//   const display = weather(city, weatherData);
-//   response.status(200).json(display);
+  //   const display = weather(city, weatherData);
+  //   response.status(200).json(display);
 
   const finalWeatherArray = weatherArray.map(day => {
-      return new Weather(day);
-  })
+    return new Weather(day);
+  });
   response.send(finalWeatherArray);
 });
 
@@ -60,13 +76,18 @@ function Location (city, geoData) {
 // }
 
 function Weather(obj){
-    this.forecast = obj.summary;
-    this.time = new Date(obj.time * 1000).toDateString();
+  this.forecast = obj.summary;
+  this.time = new Date(obj.time * 1000).toDateString();
+}
+
+function handleError(error, request, response) {
+  response.status(500).send({
+    status: 500,
+    responseText: 'Sorry, something went wrong',
+  });
 }
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
-
-
 
 app.listen (PORT, () => {
   console.log('App is running on PORT: ' + PORT);
