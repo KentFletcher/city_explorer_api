614ab771106385ceceb7e85d8283e5d19d8af17b
KentFletcher <kentfletch09@gmail.com>
2020-04-18 12:42:03 -0700

    completed lab 08 feature 2, writing location to database

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE locations(
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude INTEGER NOT NULL,
-    longitude INTEGER NOT NULL
-)
+    latitude NUMERIC(10,7),
+    longitude NUMERIC(10,7)
+);
 
